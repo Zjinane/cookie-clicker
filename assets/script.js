664b3ad5299a94bcaad33265842481433c28cf3f
Guillaume Dery <dery.guillaume98@outlook.fr>
2019-09-24 14:16:36 +0200

    Ajout muliplicateur + auto-click

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,59 @@
 var click = 1;
 var score = 0;
 var prixMulti = 10;
-
+var auto = 0;
+var prixAuto = 50;
+var multiAuto = 0;
 
 var btnMulti = document.getElementById("btnMulti");
 var afficheScore = document.getElementById("afficheScore");
 var btnClick = document.getElementById("btnClick");
 var multi = document.getElementById("multi");
 var affichePrixMulti = document.getElementById("affichePrixMulti");
+var affichePrixAuto = document.getElementById("affichePrixAuto");
+var afficheAuto = document.getElementById("afficheAuto");
+var btnAuto = document.getElementById("btnAuto");
 
+affichePrixMulti.innerHTML = "Multiplicateur (prix = " + prixMulti + ")";
+affichePrixAuto.innerHTML = "Auto-click (prix = " + prixAuto + ")";
 
-btnClick.addEventListener("click", function(){
-
-	score = score + click ;
-	afficheScore.innerHTML = score;
-
+btnClick.addEventListener("click", function() {
+    score = score + click;
+    afficheScore.innerHTML = score;
 });
 
-btnMulti.addEventListener("click",function(){
-
-	click = click + 1 ;
-	score = score - prixMulti;
-	prixMulti = prixMulti * 2 ;
-	multi.innerHTML = click;
-	afficheScore.innerHTML = score;
-
+btnMulti.addEventListener("click", function() {
+    var testeurScore = score - prixMulti;
+    if (testeurScore >= 0) {
+        click = click + 1;
+        score = score - prixMulti;
+        prixMulti = prixMulti * 2;
+        multi.innerHTML = click;
+        afficheScore.innerHTML = score;
+        affichePrixMulti.innerHTML = "Multiplicateur (prix = " + prixMulti + ")";
+    } else {
+        alert("Il te faut plus de cliques !!!");
+    }
 });
 
+btnAuto.addEventListener("click", function() {
+    var testeurScore = score - prixAuto;
+    if (testeurScore >= 0) {
+        score = score - prixAuto;
+        afficheScore.innerHTML = score;
+        auto = auto + 1;
+        prixAuto = prixAuto * 2;
+        afficheAuto.innerHTML = auto;
+        affichePrixAuto.innerHTML = "Auto-click (prix = " + prixAuto + ")";
+    } else {
+        alert("Il te faut plus de cliques !!!");
+    }
+});
 
+function autoclick() {
+    multiAuto = click * auto;
+    score = score + multiAuto;
+    afficheScore.innerHTML = score;
+}
+
+setInterval(autoclick, 1000);
