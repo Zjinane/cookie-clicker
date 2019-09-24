029a1fe057e5dab4ea0c06320e6fc8c9f06b821d
Zjinane <zekji@hotmail.com>
2019-09-24 11:15:53 +0200

    add js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,30 @@
-j
+var click = 1;
+var score = 0;
+var prixMulti = 10;
+
+
+var btnMulti = document.getElementById("btnMulti");
+var afficheScore = document.getElementById("afficheScore");
+var btnClick = document.getElementById("btnClick");
+var multi = document.getElementById("multi");
+var affichePrixMulti = document.getElementById("affichePrixMulti");
+
+
+btnClick.addEventListener("click", function(){
+
+	score = score + click ;
+	afficheScore.innerHTML = score;
+
+});
+
+btnMulti.addEventListener("click",function(){
+
+	click = click + 1 ;
+	score = score - prixMulti;
+	prixMulti = prixMulti * 2 ;
+	multi.innerHTML = click;
+	afficheScore.innerHTML = score;
+
+});
+
+
