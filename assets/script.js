3680e4ead1f5675a7689ee78e259b0b36f091ccd
Guillaume Dery <dery.guillaume98@outlook.fr>
2019-09-25 10:53:31 +0200

    Suppression du code inutile

--- assets/script.js
+++ assets/script.js
@@ -21,38 +21,32 @@ affichePrixAuto.innerHTML = "Auto-click (prix = " + prixAuto + ")";
 
 btnClick.addEventListener("click", function() {
     score = score + click;
-    afficheScore.innerHTML = "Vous avez "+score+" planètes";
+    afficheScore.innerHTML = "Vous avez " + score + " planètes";
 });
 
 btnMulti.addEventListener("click", function() {
-    var testeurScore = score - prixMulti;
-    if (testeurScore >= 0) {
-        click = click + 1;
-        score = score - prixMulti;
-        prixMulti = prixMulti * 2;
-        multi.innerHTML = "Vos cliques sont multipliés par "+click;
-        afficheScore.innerHTML = "Vous avez "+score+" planètes";
-        affichePrixMulti.innerHTML = "Multiplicateur (prix = " + prixMulti + ")";
-    } else {
-        alert("Il te faut plus de cliques !!!");
-    }
+    click = click + 1;
+    score = score - prixMulti;
+    prixMulti = prixMulti * 2;
+    multi.innerHTML = "Vos cliques sont multipliés par " + click;
+    afficheScore.innerHTML = "Vous avez " + score + " planètes";
+    affichePrixMulti.innerHTML = "Multiplicateur (prix = " + prixMulti + ")";
 });
 
 btnAuto.addEventListener("click", function() {
     var testeurScore = score - prixAuto;
-       (testeurScore >= 0) 
-        score = score - prixAuto;
-        afficheScore.innerHTML = score;
-        auto = auto + 1;
-        prixAuto = prixAuto * 2;
-        afficheAuto.innerHTML = "Vous avez "+auto+" cliques auto par secondes";
-        affichePrixAuto.innerHTML = "Auto-click (prix = " + prixAuto + ")";
-    
+    testeurScore >= 0;
+    score = score - prixAuto;
+    afficheScore.innerHTML = score;
+    auto = auto + 1;
+    prixAuto = prixAuto * 2;
+    afficheAuto.innerHTML = "Vous avez " + auto + " cliques auto par secondes";
+    affichePrixAuto.innerHTML = "Auto-click (prix = " + prixAuto + ")";
 });
 
 function autoclick() {
     score = score + auto;
-    afficheScore.innerHTML = "Vous avez "+score+" planètes";
+    afficheScore.innerHTML = "Vous avez " + score + " aliens";
 }
 
 setInterval(autoclick, 1000);
@@ -66,62 +60,48 @@ function bonus() {
 }
 
 btnBonus.addEventListener("click", function() {
-
-if(score >= 100){
-	i=30;
-    compte = setInterval(decompte,1000);
-    click = click*2;
-    auto = auto*2;
-	bonus = true;
-    score = score-100;
-	btnMulti.disabled = true ;
-	btnAuto.disabled = true ; 
-}
-else
-{
-alert("Il vous faut plus de cliques");
-}
-
+    i = 30;
+    compte = setInterval(decompte, 1000);
+    click = click * 2;
+    auto = auto * 2;
+    bonus = true;
+    score = score - 100;
+    btnMulti.disabled = true;
+    btnAuto.disabled = true;
 });
-function decompte(){
-    afficheBonus.innerHTML = "Temps restant : "+i+" sec";
+
+function decompte() {
+    afficheBonus.innerHTML = "Temps restant : " + i + " sec";
     i--;
-    if(i<=0){
+    if (i <= 0) {
         clearInterval(compte);
         afficheBonus.innerHTML = "Aucun bonus en cours";
-        click = Math.round(click/2);
-        auto = Math.round(auto/2);
-		bonus = false;
-		btnMulti.disabled = false;
-		btnAuto.disabled = false;
-		multi.innerHTML = click ;
-		afficheAuto.innerHTML = auto;
-}
+        click = Math.round(click / 2);
+        auto = Math.round(auto / 2);
+        bonus = false;
+        btnMulti.disabled = false;
+        btnAuto.disabled = false;
+    }
 }
 
-function hideBtn(){
-if(bonus == false){
-
-if(score < prixMulti){
-	btnMulti.disabled = true ;
-}
-else{
-	btnMulti.disabled = false ;
-}
-if(score < prixAuto){
-	btnAuto.disabled = true ;
-}
-else{
-	btnAuto.disabled = false ;
-}
-if(score < 100){
-	btnBonus.disabled = true;
-}
-else{
-	btnBonus.disabled = false ;
-}
-}
+function hideBtn() {
+    if (bonus == false) {
+        if (score < prixMulti) {
+            btnMulti.disabled = true;
+        } else {
+            btnMulti.disabled = false;
+        }
+        if (score < prixAuto) {
+            btnAuto.disabled = true;
+        } else {
+            btnAuto.disabled = false;
+        }
+        if (score < 100) {
+            btnBonus.disabled = true;
+        } else {
+            btnBonus.disabled = false;
+        }
+    }
 }
 
-setInterval(hideBtn,100);
-
+setInterval(hideBtn, 100);
